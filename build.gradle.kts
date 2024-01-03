28e14db79dde92da97cec08df2f34271cf5b3f5f
MILU0000 <91049913+MILU0000@users.noreply.github.com>
2024-01-03 02:56:55 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     autowire(libs.plugins.com.android.application) apply false
     autowire(libs.plugins.org.jetbrains.kotlin.android) apply false
 }
+
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
